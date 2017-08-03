5c825059ea6e0ba68eea2ab3d5542a129fc8f782
Samir Dhebar <samirdhebar@gmail.com>
2017-08-02 23:24:10 -0400

    scaffolding for AllItems page

--- src/pages/AllItems.jsx
+++ src/pages/AllItems.jsx
@@ -1,4 +1,17 @@
 // only required here. This will only be passed info for other pages.
+import React, { Component } from "react";
 import RAWPRODUCTS from "json/products.json";
 
+
+class AllItems extends Component {
+	render() {
+		return (
+			<div>
+				{/* <pre>{JSON.stringify(RAWPRODUCTS, null, 2)}</pre> */}
+			</div>
+		);
+	}
+}
+
+export default AllItems;
 // Check Panopto and Gist
