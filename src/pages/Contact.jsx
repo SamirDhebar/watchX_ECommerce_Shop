cc85bf0005de98573c438d0b1f3bc3cc4be17da6
Samir Dhebar <samirdhebar@gmail.com>
2017-08-02 23:55:58 -0400

    created contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello, I'm Contact!</h1>
+			</div>
+		);
+	}
+}
+
+export default Contact;
