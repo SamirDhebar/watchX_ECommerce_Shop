9206f4e013029aad69305851c16d5706db603293
Samir Dhebar <samirdhebar@gmail.com>
2017-08-12 18:40:00 -0400

    added checkout components, styling still needed

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,58 @@
+import "./Checkout.scss";
+import React, { Component } from "react";
+
+class Checkout extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			customerName: "",
+			customerEmail: "",
+			customerPhone: 0,
+			customerAddress: "",
+			customerState: "",
+			customerZip: 0,
+			isValid: false,
+		};
+	}
+	render() {
+  		const { cartTotal } = this.props;
+
+  		return (
+  			<div className="checkout-form-container">
+  				<form action="/Success">
+  					<div className="button-field">
+  						<label name="userName">Username</label>
+  						<input type="text" name="userName" required />
+  					</div>
+  					<div className="button-field">
+  						<label name="userEmail">userEmail</label>
+  						<input type="text" name="userEmail" required />
+  					</div>
+  					<div className="button-field">
+  						<label name="userPhone">Phone</label>
+  						<input type="text" name="userPhone" required />
+  					</div>
+  					<div className="button-field">
+  						<label name="userAddress">Street address</label>
+  						<input type="text" name="userAddress" required />
+  					</div>
+  					<div className="button-field">
+  						<label name="userState">State</label>
+  						<input type="text" name="userState" required />
+  					</div>
+  					<div className="button-field">
+  						<label name="userZip">Zip code</label>
+  						<input type="text" name="userZip" required />
+  					</div>
+  					<div className="button-field-submit hidden">
+  						<label name="submit" >Submit</label>
+  						<button type="submit" />
+  					</div>
+
+  				</form>
+  			</div>
+  		);
+  	}
+}
+
+export default Checkout;
