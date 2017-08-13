bc9e15760e95a2bd7c4b430ff99b77f84513cd82
Samir Dhebar <samirdhebar@gmail.com>
2017-08-13 17:23:08 -0400

    added background pic for landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
+import "./Home.scss";
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Hello, I'm Home!</h1>
+			<div className="app-home">
+				<div className="container">
+					<div className="image-container"  />
+					<div className="info-container" >
+
+								Catalog
+					</div>
+
+				</div>
 			</div>
+
+
 		);
 	}
 }
