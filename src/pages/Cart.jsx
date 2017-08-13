c2340c9a90c7e0589eb5a66541c0e58b7c2ce2a0
Samir Dhebar <samirdhebar@gmail.com>
2017-08-13 11:56:51 -0400

    cart can now render if the cart is empty

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-
 class Cart extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			// cartTotal: 0,
+		};
+	}
 	render() {
-		return (
-			<div>
-				<h1>Hello, I'm Cart!</h1>
-			</div>
-		);
+		const { cart, cartCount } = this.props;
+		if (cartCount > 0) {
+			return (
+				<div className="app">
+					<h1>Cart</h1>
+					<ul>
+						{cart.map((item) => (
+							<div className="items-container">
+								<img src={item.images[0].small} />
+								<h1>{item.name}</h1>
+								<h3>{item.price}</h3>
+							</div>
+						))}
+					</ul>
+					<div className="cart-count">{ cartCount } items in your Cart</div>
+				</div>
+			);
+		}
+		else {
+			return (
+				<div className="empty-cart">
+					<h1>Your cart is empty</h1>
+				</div>
+			);
+		}
 	}
 }
-
 export default Cart;
